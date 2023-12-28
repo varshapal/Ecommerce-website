5b097eaf5672bc8a5ca651f35cdf6df20f6f6726
varshapal <varsha25.pal@gmail.com>
2023-12-28 11:58:28 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+import { Container, Navbar } from "react-bootstrap";
+import Products from "./components/Layout/Products";
+
+const App = () => {
+  
+  
+  return (
+    <div>
+      <Products />
+      
+    </div>
+  );
+}
+
+export default App;
